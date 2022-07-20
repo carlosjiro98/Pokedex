27711da6375f537dcf96b0cd97ddff750946456b
calrosjiro98 <carlosabjiro@hotmail.com>
2022-07-20 06:08:02 -0500

    cool thinga

--- api/src/constrollers/mainRouteParser.js
+++ api/src/constrollers/mainRouteParser.js
@@ -3,16 +3,27 @@ function mainRouteParser (pokeDatos) {
     let pokeInstance = {}
     if(typeof(pokeDatos.id) === "number"){
         let types = pokeDatos.types.map((e) => e.type.name)
+        let stats = pokeDatos.stats.map((e)=>{
+            let name = e.stat.name
+            return {
+                name: name,
+                base: e.base_stat
+            }
+        })
         pokeInstance = {
+            id: pokeDatos.id,
             image: pokeDatos.sprites.other.dream_world.front_default || "no hay imagen",//pokeDatos.sprites.front_default,
             name: pokeDatos.name,
+            attack: stats.find((e)=>e.name === "attack").base,
             types: types
         }
     } else {
         let types = pokeDatos.types.map((e) => e.name)
         pokeInstance = {
+            id: pokeDatos.id,
             image: pokeDatos.image || "no hay imagen",//pokeDatos.sprites.front_default,
             name: pokeDatos.name,
+            attack: pokeDatos.attack,
             types: types
         }
     }
